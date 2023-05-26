790322ebd1a5e046a8af46466f48be19377e144a
Brian Yu <brianyu@BriandeMacBook-Pro.local>
2023-05-26 14:26:13 -0700

    a3 fixes

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -13,10 +13,10 @@ function Tuiter() {
    <div>
      <Nav />
      <div className="row">
-       <div className="col-2">
+       <div className="col-2 col-sm-2 col-md-2 col-lg-1 col-xl-2 col-xxl-2">
          <NavigationSidebar />
        </div>
-       <div className="col-7">
+       <div className="col-10 col-sm-10 col-md-10 col-lg-7 col-xl-7 col-xxl-7">
          <Routes>
            <Route path="/home" element={<HomeScreen />} />
            <Route path="/explore" element={<ExploreScreen />} />
@@ -24,7 +24,7 @@ function Tuiter() {
            <Route path="/profile" element={<ProfileScreen />} />
          </Routes>
        </div>
-       <div className="col-3">
+       <div className="col-0 col-sm-0 col-md-0 col-lg-4 col-xl-3 col-xxl-3">
          <WhoToFollowList />
        </div>
      </div>
